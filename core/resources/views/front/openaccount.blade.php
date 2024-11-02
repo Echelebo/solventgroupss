e8153e4e8b2f8e62c0746706c76131cf569feb56
Echelebo <echelebochibuike@gmail.com>
2024-11-02 07:41:26 +0100

    First

--- core/resources/views/front/openaccount.blade.php
+++ core/resources/views/front/openaccount.blade.php
@@ -185,7 +185,10 @@
                                                                             Focus on Banking Portals</h3>
                                                                     </div>
                                                                 </div>
-
+                                                                <div class="elementor-element elementor-element-4888aa7 cmsmasters-tabs-responsive-tablet-x cmsmasters-list-ver-align-center cmsmasters-tabs-type-horizontal cmsmasters-tab-list-mode-inline cmsmasters-list-item-alignment-left cmsmasters-icon-position-left cmsmasters-tabs-position-start cmsmasters-icon-view-default cmsmasters-pointer-none cmsmasters-block-default cmsmasters-sticky-default elementor-widget elementor-widget-cmsmasters-tabs cmsmasters-widget-tabs"
+                                                                    data-id="4888aa7" data-element_type="widget"
+                                                                    data-settings=""
+                                                                    data-widget_type="cmsmasters-tabs.default">
                                                                     <div class="elementor-widget-container">
                                                                         <div class="cmsmasters-tabs" role="tablist">
                                                                             <div class="cmsmasters-tabs-list-wrapper">
@@ -224,7 +227,7 @@
 
                                                                         </div>
                                                                     </div>
-
+                                                                </div>
                                                                 <div class="elementor-element elementor-element-6489693 cmsmasters-block-default cmsmasters-sticky-default elementor-widget elementor-widget-text-editor"
                                                                     data-id="6489693" data-element_type="widget"
                                                                     data-widget_type="text-editor.default">
