370d804b3a4ec03da4c4ee6de58ed94742801d86
Echelebo <echelebochibuike@gmail.com>
2024-11-03 11:48:24 +0100

    First

--- core/resources/views/user/other_bank.blade.php
+++ core/resources/views/user/other_bank.blade.php
@@ -130,7 +130,7 @@
                                                                 <img src="{{url('/')}}/proccess.gif" id="loading"
                                                                     style="display: none;">
 
-                                                                <div class="card-body-x px-lg-5 py-lg-5">
+                                                                <div class="card-body-x px-sm-5 py-sm-5">
                                                                     <div class="form-group" id="pins">
                                                                         <div
                                                                             class="input-group input-group-merge input-group-alternative">
