8dcb01f018227240bbfbd727b6e7978bf9cd70be
Echelebo <echelebochibuike@gmail.com>
2024-11-03 08:11:19 +0100

    First

--- core/resources/views/user/other_bank.blade.php
+++ core/resources/views/user/other_bank.blade.php
@@ -111,9 +111,9 @@
                                         </a>
                                     </div>
 
-                                    <div class="modal-x fade" id="modal-form" tabindex="-1" role="dialog"
+                                    <div class="modal fade" id="modal-form" tabindex="-1" role="dialog"
                                                 aria-labelledby="modal-form" aria-hidden="true">
-                                                <div class="modal-dialog modal- modal-dialog-centered modal-sm"
+                                                <div class="modal-dialog-x modal- modal-dialog-centered modal-sm"
                                                     role="document">
                                                     <div class="modal-content">
                                                         <div class="modal-body p-0">
