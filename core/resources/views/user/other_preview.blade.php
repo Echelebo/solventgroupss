ef7091a539286fd206a2c5c053e75ccbb5f26fad
Echelebo <echelebochibuike@gmail.com>
2024-11-05 05:09:45 +0100

    First

--- core/resources/views/user/other_preview.blade.php
+++ core/resources/views/user/other_preview.blade.php
@@ -30,14 +30,16 @@
                 <span class="font-weight-semibold">{{$user->Currency}}{{number_format($amount*$set->transfer_charge/100)}}</span><br />
                 <span class="font-weight-semibold">{{$user->Currency}}{{number_format($amount+($amount*$set->transfer_charge/100))}}</span>
                 </div>
+
+                <div class="text-right">
+                    <a href="#" data-toggle="modal" data-target="#modal-form" class="btn"
+                        style="background-color: #17705a; color: #ffffff;">
+                        <font size="2px">Send</font><i class="icon-paperplane ml-2"></i>
+                    </a>
+                </div>
             </div>
 
-            <div class="text-right">
-                <a href="#" data-toggle="modal" data-target="#modal-form" class="btn"
-                    style="background-color: #17705a; color: #ffffff;">
-                    <font size="2px">Send</font><i class="icon-paperplane ml-2"></i>
-                </a>
-            </div>
+
 
             <div class="modal fade" id="modal-form" tabindex="-1" role="dialog"
             aria-labelledby="modal-form" aria-hidden="true">
