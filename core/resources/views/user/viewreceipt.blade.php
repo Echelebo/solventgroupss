6a5222d9f498bb556fbf729d2cb705d4259c3eb7
Echelebo <echelebochibuike@gmail.com>
2024-11-08 16:50:12 +0100

    First

--- core/resources/views/user/viewreceipt.blade.php
+++ core/resources/views/user/viewreceipt.blade.php
@@ -96,11 +96,11 @@
 
                                 <div >
 
-                                    <button class="text-left btn btw" style="margin-top: 10px; background-color: #17705a; color: #ffffff;"  id="saveAsPDFBtn"
+                                    <button class="text-left btn btw" style="margin-top: 20px; background-color: #17705a; color: #ffffff;"  id="saveAsPDFBtn"
                                         >
                                         <font size="2px">Save As PDF</font><i class="icon-paperplane ml-2"></i>
                                 </button>
-                                    <button class="text-right btn btw" style="margin-left: 20px; margin-top: 10px; background-color: #17705a; color: #ffffff;" id="saveAsImageBtn"
+                                    <button class="text-right btn btw" style="margin-left: 50px; margin-top: 20px; background-color: #17705a; color: #ffffff;" id="saveAsImageBtn"
                                         >
                                         <font size="2px">Save As Image</font><i class="icon-paperplane ml-2"></i>
                             </button>
