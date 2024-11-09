bcf54b72f8e15b21a2644cea465fc26347d93248
Echelebo <echelebochibuike@gmail.com>
2024-11-09 14:53:10 +0100

    First

--- core/resources/views/user/viewreceipt.blade.php
+++ core/resources/views/user/viewreceipt.blade.php
@@ -1,112 +1,120 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js">
+</script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
 
 @extends('userlayout')
 
 @section('content')
-<main class="page-content" style="margin-top:40px">
-<div class="container-fluid mt--6">
-  <div class="content-wrapper">
-  <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12">
-      <div class="card-x bg-white">
-        <div class="card-body-x">
-            @foreach ($reference as $hh)
-            <div class="text-center" style="width: 40px; margin: auto;" ><img src="{{url('/')}}/newe/img/mark.png" / >
-            </div>
-            <h4 class="text-center mt-5">Successful</h4>
-            @if ($hh->type == 1)
-            <h3 class="text-center" style="color:darkred;"><b>- {{$user->Currency}}{{number_format($hh->amount)}}</b></h3>
-            @elseif($hh->type == 2)
-            <h3 class="text-center" style="color:#17705a;"><b>{{$user->Currency}}{{number_format($hh->amount)}}</b></h3>
-            @endif
-<p class="text-center">
-    @if ($hh->valuex == 1)
-    {{ $hh->dates }}
-@elseif($hh->valuex == 0)
-    {{ $hh->created_at }}
-@endif</p>
-
-
-            <div class="text-right" style="margin-right: 5px; margin-top: 100px;">
-                <br />
-                <a href="#" data-toggle="modal" data-target="#modal-form{{ $hh->reference }}" class="btn"
-                    style="background-color: #17705a; color: #ffffff;">
-                    <font size="2px">View Receipt</font><i class="icon-paperplane ml-2"></i>
-                </a>
-            </div>
-
-            <div class="modal fade" id="modal-form{{ $hh->reference }}" tabindex="-1" role="dialog"
-            aria-labelledby="modal-form" aria-hidden="true">
-            <div class="modal-dialog modal- modal-dialog-centered modal-sm"
-                role="document">
-                <div class="modal-content" id="modalContent">
-                    <div class="modal-body p-0">
-                        <div class="card-x bg-white border-0 mb-0">
-<div class="card-body-x p-5">
-
-    <div class="text-left" style="width: 100px; margin-left: 5px; " ><img src="{{url('/')}}/asset/{{ $logo->image_link }}" / >
-    </div>
-
-    @if ($hh->type == 1)
-    <h3 class="text-center mt-5" style="color:darkred;"><b>- {{$user->Currency}}{{number_format($hh->amount)}}</b></h3>
-    @elseif($hh->type == 2)
-    <h3 class="text-center mt-5" style="color:#17705a;"><b>{{$user->Currency}}{{number_format($hh->amount)}}</b></h3>
-    @endif
-
-    <h4 class="text-center">Successful Transaction</h4>
-<p class="text-center">
-@if ($hh->valuex == 1)
-{{ $hh->dates }}
-@elseif($hh->valuex == 0)
-{{ $hh->created_at }}
-@endif</p>
-                                <div class="conspbtw" style="width: 90%; margin: auto;">
-
-
-
-                                    <div class="lefutu">
+    <main class="page-content" style="margin-top:40px">
+        <div class="container-fluid mt--6">
+            <div class="content-wrapper">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                        <div class="card-x bg-white">
+                            <div class="card-body-x">
+                                @foreach ($reference as $hh)
+                                    <div class="text-center" style="width: 40px; margin: auto;"><img
+                                            src="{{ url('/') }}/newe/img/mark.png" />
+                                    </div>
+                                    <h4 class="text-center mt-5">Successful</h4>
                                     @if ($hh->type == 1)
-                                    <span>Sender: </span><br />
-                                    <span>Recipient: </span><br />
+                                        <h3 class="text-center" style="color:darkred;"><b>-
+                                                {{ $user->Currency }}{{ number_format($hh->amount) }}</b></h3>
                                     @elseif($hh->type == 2)
-                                    <span>Recipient: </span><br />
-                                    <span>Sender: </span><br />
+                                        <h3 class="text-center" style="color:#17705a;">
+                                            <b>{{ $user->Currency }}{{ number_format($hh->amount) }}</b></h3>
                                     @endif
-                                    @if ($hh->type == 1)
-                                                                <span>Recipient Bank:</span><br />
+                                    <p class="text-center">
+                                        @if ($hh->valuex == 1)
+                                            {{ $hh->dates }}
+                                        @elseif($hh->valuex == 0)
+                                            {{ $hh->created_at }}
+                                        @endif
+                                    </p>
+                                    <div class="text-right" style="margin-right: 5px; margin-top: 100px;">
+                                        <br />
+                                        <a href="#" data-toggle="modal" data-target="#modal-form{{ $hh->reference }}"
+                                            class="btn" style="background-color: #17705a; color: #ffffff;">
+                                            <font size="2px">View Receipt</font><i class="icon-paperplane ml-2"></i>
+                                        </a>
+                                    </div>
+
+                                    <div class="modal fade" id="modal-form{{ $hh->reference }}" tabindex="-1"
+                                        role="dialog" aria-labelledby="modal-form" aria-hidden="true">
+                                        <div class="modal-dialog modal- modal-dialog-centered modal-sm" role="document">
+                                            <div class="modal-content" id="modalContent">
+                                                <div class="modal-body p-0">
+                                                    <div class="card-x bg-white border-0 mb-0">
+                                                        <div class="card-body-x p-5">
+
+                                                            <div class="text-left" style="width: 100px; margin-left: 5px; ">
+                                                                <img src="{{ url('/') }}/asset/{{ $logo->image_link }}"
+                                                                    />
+                                                            </div>
+
+                                                            @if ($hh->type == 1)
+                                                                <h3 class="text-center mt-5" style="color:darkred;"><b>-
+                                                                        {{ $user->Currency }}{{ number_format($hh->amount) }}</b>
+                                                                </h3>
                                                             @elseif($hh->type == 2)
-                                                                <span>Sender Bank:</span><br />
+                                                                <h3 class="text-center mt-5" style="color:#17705a;">
+                                                                    <b>{{ $user->Currency }}{{ number_format($hh->amount) }}</b>
+                                                                </h3>
                                                             @endif
-                                    <span>Reference: </span><br />
-                                    <span>Amount: </span><br />
-                                    </div>
-                                    <div class="rightu">
-                                        @if ($hh->type == 1)
-                                    <span>{{ $hh->account }}</span><br />
-                                    <span>{{ $hh->sender_acctno }}</span><br />
-                                    @elseif($hh->type == 2)
-                                    <span>{{ $hh->account }}</span><br />
-                                    <span>{{ $hh->sender_acctno }}</span><br />
-                                    @endif
-                                    <span>{{ $hh->sender_bankname }}</span><br />
-                                    <span>{{ $hh->reference }}</span><br />
-                                    <span>{{ $user->Currency }}{{ number_format($hh->amount, 2) }}</span><br />
-                                    </div>
-                                </div>
+
+                                                            <h4 class="text-center">Successful Transaction</h4>
+                                                            <p class="text-center">
+                                                                @if ($hh->valuex == 1)
+                                                                    {{ $hh->dates }}
+                                                                @elseif($hh->valuex == 0)
+                                                                    {{ $hh->created_at }}
+                                                                @endif
+                                                            </p>
+                                                            <div class="conspbtw" style="width: 90%; margin: auto;">
+
+
+
+                                                                <div class="lefutu">
+                                                                    @if ($hh->type == 1)
+                                                                        <span>Sender: </span><br />
+                                                                        <span>Recipient: </span><br />
+                                                                    @elseif($hh->type == 2)
+                                                                        <span>Recipient: </span><br />
+                                                                        <span>Sender: </span><br />
+                                                                    @endif
+                                                                    @if ($hh->type == 1)
+                                                                        <span>Recipient Bank:</span><br />
+                                                                    @elseif($hh->type == 2)
+                                                                        <span>Sender Bank:</span><br />
+                                                                    @endif
+                                                                    <span>Reference: </span><br />
+                                                                    <span>Amount: </span><br />
+                                                                </div>
+                                                                <div class="rightu">
+                                                                    @if ($hh->type == 1)
+                                                                        <span>{{ $hh->account }}</span><br />
+                                                                        <span>{{ $hh->sender_acctno }}</span><br />
+                                                                    @elseif($hh->type == 2)
+                                                                        <span>{{ $hh->account }}</span><br />
+                                                                        <span>{{ $hh->sender_acctno }}</span><br />
+                                                                    @endif
+                                                                    <span>{{ $hh->sender_bankname }}</span><br />
+                                                                    <span>{{ $hh->reference }}</span><br />
+                                                                    <span>{{ $user->Currency }}{{ number_format($hh->amount, 2) }}</span><br />
+                                                                </div>
+                                                            </div>
                                 @endforeach
 
 
-                                <div >
+                                <div>
 
-                                    <button class="text-left btn" style="margin-top: 20px; background-color: #17705a; color: #ffffff;"  id="saveAsPDFBtn">
-                                        <font size="2px">Save As PDF</font><i class="icon-paperplane ml-2"></i>
-                                </button>
-                                    <button class="text-right btn" style="margin-left: 50px; margin-top: 20px; background-color: #17705a; color: #ffffff;" id="saveAsImageBtn"
-                                        >
-                                        <font size="2px">Save As Image</font><i class="icon-paperplane ml-2"></i>
-                            </button>
+                                    <button class="text-left btn"
+                                        style="margin-top: 20px; background-color: #17705a; color: #ffffff;"
+                                        onclick="window.print();">
+                                        <font size="2px">Share Receipt</font><i class="icon-paperplane ml-2"></i>
+                                    </button>
+
                                 </div>
 
 
@@ -115,40 +123,19 @@
 
                             </div>
 
+                        </div>
+                    </div>
                 </div>
+
             </div>
         </div>
 
         </div>
-      </div>
-
-  </div>
-  </div>
-  </div>
-  </main>
-
-  <script>
-    document.getElementById('saveAsImageBtn').addEventListener('click', function() {
-    var element = document.getElementById('modalContent'); // Your modal content ID
-    html2canvas(element).then(function(canvas) {
-        var link = document.createElement('a');
-        link.download = 'receipt.png';
-        link.href = canvas.toDataURL('image/png');
-        link.click();
-    });
-});
+        </div>
+        </div>
+    </main>
 
 
-  </script>
-  <script>
-document.getElementById('saveAsPDFBtn').addEventListener('click', function() {
-    var element = document.getElementById('modalContent'); // Your modal content ID
-
-    const pdf = new jsPDF();
-    pdf.fromHTML(element.innerHTML);
-    pdf.save('receipt.pdf');
-});
-  </script>
 
 
 @stop
